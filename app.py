9d1efd9688377572512feca593ce6cb13eb201b2
Rachell Ng <rachellnyr@gmail.com>
2022-04-20 00:00:17 +0800

    Add streamlit page modules

--- app.py
+++ app.py
@@ -7,35 +7,64 @@ import configparser
 
 import graphistry
 
+from pages import graphistry_page
+from pages import map_page
+from pages import bus_page
+from pages import train_page
+from pages import trip_page
+from pages import simulation_page
+
 #from functions import graph_overview
 
 PAGE_CONFIG = {"page_title":"PlaNET","page_icon":":tiger:","layout":"wide"}
 st.set_page_config(**PAGE_CONFIG)
 
 
-def connect(config_path):
-    config = configparser.ConfigParser()
-    config.read(config_path)
+# def connect(config_path):
+#     config = configparser.ConfigParser()
+#     config.read(config_path)
     
-    conn = tg.TigerGraphConnection(host=config['tigergraph']['host'], 
-                                   username=config['tigergraph']['username'],
-                                   password=config['tigergraph']['password'], 
-                                   useCert=True)
+#     conn = tg.TigerGraphConnection(host=config['tigergraph']['host'], 
+#                                    username=config['tigergraph']['username'],
+#                                    password=config['tigergraph']['password'], 
+#                                    useCert=True)
 
-    graphistry.register(api=3, protocol="https", 
-                        server=config['graphistry']['server'], 
-                        username=config['graphistry']['username'], 
-                        password=config['graphistry']['password'])
-    return conn
+#     graphistry.register(api=3, protocol="https", 
+#                         server=config['graphistry']['server'], 
+#                         username=config['graphistry']['username'], 
+#                         password=config['graphistry']['password'])
+#     return conn
 
 
 def main():
-    st.title("Tigyore Graph")
+    st.sidebar.title("PlaNET")
 
-    pages = ["Graphistry", "Map View", "Bus Overview", "Train Overview", "Trip Drilldown", "Simulation"]
-    conn = connect('configs.ini')
-    selected_page = st.sidebar.selectbox('Pages',pages)
+    pages = ["About", "Graphistry", "Map View", "Bus Overview", "Train Overview", "Trip Drilldown", "Simulation"]
+    # conn = connect('configs.ini')
+    page = st.sidebar.radio('Navigate', 
+                        options=pages, # geospatial
+                        index=0)
+    # selected_page = st.sidebar.selectbox('Pages',pages)
     
+    # # Pages
+    if page.lower() == 'about':
+        about = open('README.md', 'r')
+        # print(about.read())
+        st.markdown(about.read())
+    elif page.lower() == 'graphistry':
+        graphistry_page.render()
+    elif page.lower() == 'map view':
+        map_page.render()
+    elif page.lower() == 'bus overview':
+        bus_page.render()
+    elif page.lower() == 'train overview':
+        train_page.render()
+    elif page.lower() == 'trip drilldown':
+        trip_page.render()
+    elif page.lower() == 'simulation':
+        simulation_page.render()
+    else:
+        st.text('Page ' + page + ' is not implemented.')
 
 if __name__ == '__main__':
     main()
---
+++ pages/simulation_page.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def render():
+    # Main
+    st.subheader("Simulation")
