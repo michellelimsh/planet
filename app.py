326a4aaede89de3fa43b59fe081fa773ec496ea3
Michelle Lim <michellelimsh@gmail.com>
2022-04-20 05:22:09 +0800

    added functionality for degree centrality and jaccard similarity analysis

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ import configparser
 
 import graphistry
 
-#from functions import graph_overview
-
 PAGE_CONFIG = {"page_title":"PlaNET","page_icon":":tiger:","layout":"wide"}
 st.set_page_config(**PAGE_CONFIG)
 
@@ -18,9 +16,12 @@ def connect(config_path):
     config.read(config_path)
     
     conn = tg.TigerGraphConnection(host=config['tigergraph']['host'], 
+                                   graphname=config['tigergraph']['graph_name'],
                                    username=config['tigergraph']['username'],
-                                   password=config['tigergraph']['password'], 
-                                   useCert=True)
+                                   password=config['tigergraph']['password'])
+
+    conn.apiToken = conn.getToken(config['tigergraph']['secret'])     
+    
 
     graphistry.register(api=3, protocol="https", 
                         server=config['graphistry']['server'], 
@@ -28,14 +29,48 @@ def connect(config_path):
                         password=config['graphistry']['password'])
     return conn
 
+conn = connect('configs.ini')
+
+def run_query(query_name, params, output_name):
+    results = conn.runInstalledQuery(query_name, params=params)
+    df = pd.DataFrame(results[0][output_name])
+    return df
 
 def main():
     st.title("Tigyore Graph")
-
-    pages = ["Graphistry", "Map View", "Bus Overview", "Train Overview", "Trip Drilldown", "Simulation"]
-    conn = connect('configs.ini')
-    selected_page = st.sidebar.selectbox('Pages',pages)
+    pages = ["Centrality Analysis", "Service Similarity Analysis"]
     
+    selected_page = st.sidebar.radio('Pages',pages)
+
+    
+    if selected_page == 'Centrality Analysis':
+        option = st.selectbox('Select vertex to conduct centrality analysis on', ("TrainStation","BusStop","TrainService","BusService"))
+        limit = st.number_input(label='max to display', value=10)
+        if option == "TrainStation" or option == "BusStop":
+            df = run_query("station_degree_centrality",
+                           {'v_type':option, 'e_type':'waypoint', 'top_k':10},
+                           "top_scores"
+                            )
+        elif option == "TrainService" or option == "BusService":
+            df = run_query("service_degree_centrality",
+                           {'v_type':option, 'e_type':'utilise', 'top_k':10},
+                           "top_scores"
+                            )
+        else:
+            print("Error")
+        st.dataframe(df)
+
+    elif selected_page == 'Service Similarity Analysis':
+        option = st.selectbox('Select vertex to conduct similarity analysis on', ("TrainStation","BusStop","TrainService","BusService"))
+        selected_vertex = st.text_input(label='input id of vertex')
+        if option and selected_vertex:
+            st.write('You have selected', option, selected_vertex)
+            df = run_query("jaccard_similarity",
+                            {'source':'1', 'source.type':'TrainStation', 'e_type':'serve', 'rev_e_type':'serve', 'top_k':10},
+                            "Others"
+                            )
+            st.dataframe(df)
+
 
 if __name__ == '__main__':
     main()
